828771afe963dee10bb282ea1fb9b8ad93643f36
Sam Eubank <sameubank@gmail.com>
2008-12-14 05:11:19 +0000

    Missing templates.

---
+++ PiePHP/templates/errors/error-404.php
@@ -0,0 +1,15 @@
+<?
+require $_SERVER['DOCUMENT_ROOT'].'/../initialization/common.php';
+?>
+
+<h1>Page Not Found</h1>
+<br>
+<p>
+<?
+	echo $_SERVER['REQUEST_URI'];
+?>
+</p>
+
+<?
+PieLayout::renderPage();
+?>
