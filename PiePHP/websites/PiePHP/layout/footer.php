0800a21af87737724ad515b472f0b699c1669b12
Sam Eubank <sameubank@gmail.com>
2010-07-29 22:19:10 +0000

    Getting rid of the old PiePHP stuff. The new project will be a MVC framework loosely based on CodeIgniter.

--- PiePHP/websites/PiePHP/layout/footer.php
+++
@@ -1,8 +0,0 @@
-<? if (false) { ?><html><body><div><div><? } ?>
-		</div>
-	</div>
-<?
-PieRefresher::render();
-?>
-</body>
-</html>
--- PiePHP/websites/pie-server/webpages/configuration/index.php
+++
@@ -1,9 +0,0 @@
-<?
-require $_SERVER['DOCUMENT_ROOT'] . '/../initialization/common.php';
-?>
-
-<? phpinfo() ?>
-
-<?
-PieLayout::renderPage();
-?>
