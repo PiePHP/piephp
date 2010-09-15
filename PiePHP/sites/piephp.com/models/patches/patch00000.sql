fe5fcd1285484e125b0b9e60d3fb3d68ec7d1307
Sam Eubank <sameubank@gmail.com>
2010-09-15 09:53:11 +0000

    Added a patch to create users and user groups.

---
+++ PiePHP/sites/piephp.com/models/patches/patch00000.sql
@@ -0,0 +1,25 @@
+CREATE TABLE users (
+  id bigint(20) unsigned NOT NULL auto_increment,
+  first_name varchar(50) NOT NULL,
+  last_name varchar(50) NOT NULL,
+  email varchar(254) NOT NULL,
+  username varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  PRIMARY KEY  (id),
+  KEY first_name (first_name,last_name)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+INSERT INTO users VALUES (1, 'Sam', 'Eubank', 'sameubank@gmail.com', 'BigBear', '7c6a180b36896a0a8c02787eeafb0e4c');
+
+
+CREATE TABLE user_groups (
+  id bigint(20) unsigned NOT NULL auto_increment,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY  (id),
+  KEY `name` (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+INSERT INTO user_groups VALUES (1, 'System Administrators');
+INSERT INTO user_groups VALUES (2, 'Developers');
+INSERT INTO user_groups VALUES (3, 'Administrators');
+INSERT INTO user_groups VALUES (4, 'Moderators');
