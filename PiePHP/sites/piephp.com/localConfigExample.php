4c4c2c3d42515777491762bb58085154c90079e0
Sam Eubank <sameubank@gmail.com>
2010-10-08 12:16:19 +0000

    Added an example of a local config file that can be renamed to localConfig.php for a specific user's settings.

---
+++ PiePHP/sites/piephp.com/localConfigExample.php
@@ -0,0 +1,15 @@
+<?php
+
+$DATABASES['default'] = 'mysql:host=localhost username=YOUR_USERNAME password=YOUR_PASSWORD database=piephp';
+
+$URL_ROOT = '/index.php/';
+
+$REFRESHER_FILE = 'THE_PATH_TO_A_FILE_THAT_YOUR_EDITOR_TOUCHES_WHEN_IT_SAVES_A_FILE';
+
+$ENVIRONMENT = 'development';
+$PACKAGE = 'PiePHP';
+$COPYRIGHT = 'Copyright (c) 2007-2010, Pie Software Foundation';
+$AUTHOR = 'YOUR_NAME <YOUR_EMAIL>';
+$LICENSE = 'http://www.piephp.com/license';
+
+$SALT = 'A_SECRET_STRING_FOR_YOUR_SESSION_SECURITY';
